1c77e3672ec1c148599edec771cb7d6b535c5e23
pwhb <pwhbdev@gmail.com>
2023-10-10 15:23:45 +0630

    error handling

--- src/routes/(auth)/auth/login/+page.server.ts
+++ src/routes/(auth)/auth/login/+page.server.ts
@@ -2,47 +2,65 @@ import { fail, redirect } from '@sveltejs/kit';
 import type { Action, Actions, PageServerLoad } from './$types';
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
-export const load: PageServerLoad = async ({ locals }) => {
-	if (locals.user) {
+export const load: PageServerLoad = async ({ locals }) =>
+{
+	if (locals.user)
+	{
 		{
 			throw redirect(302, '/');
 		}
 	}
 };
 
-const login: Action = async ({ request, cookies }) => {
-	const data = await request.formData();
-	const { username, password } = Object.fromEntries(data);
+const login: Action = async ({ request, cookies }) =>
+{
+	try
+	{
+		const data = await request.formData();
+		const { username, password } = Object.fromEntries(data);
 
-	const url = `${PUBLIC_BACKEND_URL}/auth/login`;
-	const res = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			username,
-			password
-		})
-	});
+		const url = `${PUBLIC_BACKEND_URL}/auth/login`;
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				username,
+				password
+			})
+		});
 
-	const json = await res.json();
+		const json = await res.json();
 
-	if (res.ok) {
-		if (json.data.user.role === 'player') {
-			return fail(400, { message: 'Not an admin user.' });
+		if (res.ok)
+		{
+			if (json.data.user.role === 'player')
+			{
+				return fail(400, { message: 'Not an admin user.' });
+			}
+			cookies.set('user', JSON.stringify(json.data.user), {
+				path: '/'
+			});
+			cookies.set('token', json.data.token, {
+				path: '/'
+			});
+
+			throw redirect(303, '/');
+		} else
+		{
+			return fail(400, { message: json.message });
 		}
-		cookies.set('user', JSON.stringify(json.data.user), {
-			path: '/'
-		});
-		cookies.set('token', json.data.token, {
-			path: '/'
-		});
-
-		throw redirect(303, '/');
-	} else {
-		return fail(400, { message: json.message });
+	} catch (error: any)
+	{
+		console.error(error);
+		if (error.message)
+		{
+			return fail(400, { message: error.message });
+		}
+		return fail(400, { message: "something went wrong" });
 	}
+
 };
 
 export const actions: Actions = {
